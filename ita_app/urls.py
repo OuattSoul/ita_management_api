65e37ff4969192bc0321ccd5bdc40eb8563df4bc
OuattSoul <ouatt.nsoul@gmail.com>
2025-09-18 14:58:18 +0000

    employees endpoints

--- ita_app/urls.py
+++ ita_app/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet,EmployeeViewSet
 
 router = DefaultRouter()
-router.register(r'/api/users', UserViewSet, basename='user')
+router.register(r'api/users', UserViewSet, basename='user')
+router.register(r'api/employees', EmployeeViewSet, basename='employee')
 
 urlpatterns = router.urls
